4e689be93a19ac7bda46f5f5b291a1fcfb40ad89
lilia <liliakai@gmail.com>
2018-09-21 18:13:58 +0200

    add yt support

--- index.rb
+++ index.rb
@@ -9,6 +9,14 @@ bot = Cinch::Bot.new do
   end
 
 
+  on :channel, /bb yt (.*)/i do |m|
+    result = `youtube-dl --output temp.mkv --write-sub #{m.message.split.last}; cvlc temp.mkv`
+    if error
+      m.reply error
+    else
+      m.reply "now playing"
+    end
+  end
   on :channel, /bb vlc (.*)/i do |m|
     error = `cvlc #{m.message.split.last}`
     if error
